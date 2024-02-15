9a29d26c8a2f81e5f7fb2901a0cf0f4dd80b1995
Vivek Sheth <vs586@live.mdx.ac.uk>
2024-02-15 23:40:48 +0000

    changes

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,8 @@ var cacheName = "petstore-v1"; var cacheFiles = ["index.html",   //"products.js"
 ];
 
 self.addEventListener("install", function (e) {
-    console.log("[Service Worker] Install"); e.waitUntil(caches.open(cacheName).then(function (cache) {
+    console.log("[Service Worker] Install");
+    e.waitUntil(caches.open(cacheName).then(function (cache) {
         console.log("[Service Worker] Caching files"); return cache.addAll(cacheFiles);
     }
     ));
