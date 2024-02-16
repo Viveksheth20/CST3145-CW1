86cc998010c7b6aa1d590bf293cb4c7952fb85d6
Vivek Sheth <vs586@live.mdx.ac.uk>
2024-02-16 02:01:39 +0000

    changes

--- service-worker.js
+++ service-worker.js
@@ -22,3 +22,13 @@ self.addEventListener("install", function (e) {
     }
     ));
 });
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        // check if the cache has the file     
+        caches.match(e.request).then(function (r) {
+            console.log('[Service Worker] Fetching resource: ' + e.request.url);
+            // r is the matching file if it exists in the cache        
+            return r
+        }));
+});
