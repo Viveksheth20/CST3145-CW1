e078683b5a847421ef8f0c056766febcbc6711dc
Vivek Sheth <vs586@live.mdx.ac.uk>
2024-02-16 02:44:01 +0000

    c

--- service-worker.js
+++ service-worker.js
@@ -23,6 +23,7 @@ self.addEventListener("install", function (e) {
     ));
 });
 
+
 // self.addEventListener('fetch', function (e) {
 //     e.respondWith(
 //         // check if the cache has the file     
