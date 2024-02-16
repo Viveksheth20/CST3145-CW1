d21a39fc4e2ee9ce423177817f31b288d6d32930
Vivek Sheth <vs586@live.mdx.ac.uk>
2024-02-16 02:47:46 +0000

    delete

--- service-worker.js
+++ service-worker.js
@@ -23,34 +23,3 @@ self.addEventListener("install", function (e) {
     ));
 });
 
-
-// self.addEventListener('fetch', function (e) {
-//     e.respondWith(
-//         // check if the cache has the file     
-//         caches.match(e.request).then(function (r) {
-//             console.log('[Service Worker] Fetching resource: ' + e.request.url);
-//             // r is the matching file if it exists in the cache        
-//             return r
-//         }));
-// });
-
-// self.addEventListener("fetch", function (e) {
-//     e.respondWith(
-//         caches.match(e.request).then(function (cachedFile) {
-//             //if the file is in the cache, retrieve it from there         
-//             if (cachedFile) {
-//                 console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
-//                 return cachedFile;
-//             } else {
-//                 //if the file is not in the cache, download the file            
-//                 return fetch(e.request).then(function (response) {
-//                     return caches.open(cacheName).then(function (cache) {
-//                         //add the new file to the cache                  
-//                         cache.put(e.request, response.clone());
-//                         console.log("[Service Worker] Resource fetched and saved in the cache for: " + e.request.url);
-//                         return response;
-//                     });
-//                 });
-//             }
-//         }));
-// });
